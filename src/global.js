fc0fb87a309c024073b29eae0d660156976aadd2
Alexander Reyes <alexreyes64r@gmail.com>
2022-10-01 00:22:10 -0400

    Closures and Scopres

---
+++ src/global.js
@@ -0,0 +1,16 @@
+//Variables
+// Semi-Colom are not mandaatory on Js due to they will be asign later. 
+var x; //Declarate a ariable 
+var y = 'asign'; // Declarate and Asign 
+
+var x = 'Declarate once again';
+
+
+//global Scope
+var name = 'Alex'; //Global
+
+function fristName(){
+    console.log(name);
+}
+
+fristName();
